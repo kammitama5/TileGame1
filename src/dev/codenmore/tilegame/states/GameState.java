7b81a4a7d4bc033398907ed37180bbb4eff00fb3
kammitama5 <krystal.maughan@gmail.com>
2016-03-15 11:44:54 -0700

    continuation...Java
    continuing with Tile Game tut 3/15/16

---
+++ src/dev/codenmore/tilegame/states/GameState.java
@@ -0,0 +1,26 @@
+package dev.codenmore.tilegame.states;
+
+import java.awt.Graphics;
+
+import dev.codenmore.tilegame.gfx.Assets;
+
+public class GameState extends State {
+	
+	public GameState(){
+		
+	}
+
+	
+	public void tick() {
+		
+		
+	}
+
+	
+	public void render(Graphics g) {
+		
+		
+	}
+	
+
+}
